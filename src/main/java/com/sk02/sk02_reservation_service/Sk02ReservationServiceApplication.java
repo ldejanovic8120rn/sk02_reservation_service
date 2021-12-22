2d8ca767427e74ddac3a165f6c97571682d38188
Lazar Dejanovic <lazzar99@gmail.com>
2021-12-22 23:23:22 +0100

    Initial commit

---
+++ src/main/java/com/sk02/sk02_reservation_service/Sk02ReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sk02.sk02_reservation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sk02ReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sk02ReservationServiceApplication.class, args);
+    }
+
+}
